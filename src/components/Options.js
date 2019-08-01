7c0306ed6c4959ec7061595a72455b770546b7c5
efrain-vasquez <efrain.vasquez.arias777@gmail.com>
2019-08-01 00:48:28 -0500

    setup button file

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,17 +3,22 @@ import Option from './Option.js';
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove All</button>
-          {props.options.length === 0 && <p>Please add an option to get started!</p>}
-        {
-          props.options.map((option) => (
-            <Option 
-              key={option} 
-              optionText={option}
-              handleDeleteOption={props.handleDeleteOption}
-            />
-          ))
-        }
+    <button
+      className="button button--link" 
+      onClick={props.handleDeleteOptions}
+    >
+      Remove All
+    </button>
+      {props.options.length === 0 && <p>Please add an option to get started!</p>}
+      {
+        props.options.map((option) => (
+          <Option 
+            key={option} 
+            optionText={option}
+            handleDeleteOption={props.handleDeleteOption}
+          />
+       ))
+    }
   </div>
 );      
      
